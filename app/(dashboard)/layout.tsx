38c88d91061ff3bcaf9b1183c5ec49e99bd5ccc6
YusukeTsunoda <tsunotsunoda@gmail.com>
2025-02-09 20:17:52 +0900

    メインページの作成中

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Link from 'next/link';
+import Link from  'next/link';
 import { use, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { CircleIcon, Home, LogOut } from 'lucide-react';
